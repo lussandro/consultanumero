45288257cc64adfc0fb8c1780b05d0c819af997e
root <root@snep-3.snep.com.br>
2016-08-28 18:41:45 -0300

    Adicionados os arquivos de config do Projeto

---
+++ consultanumero.php
@@ -0,0 +1,71 @@
+#!/usr/bin/php -q
+<?php
+                # Consulta Operadora usando a API da Fluxo-TI  #
+                # SNEP 3.0                                     #
+                # Lussandro da Cunha Ilha                      #
+                # Palhoça 27/08/2016                           #
+
+
+        require('phpagi.php');
+	$agi = new AGI();
+	$numero = $argv[1];
+        $token = 'INSIRA O SEU TOKEN AQUI';
+        $usuario_mysql = 'snep';
+        $senha_mysql = 'sneppass';
+        $banco_mysql = 'portabilidade';
+        $conta = 99 ;
+
+        $id = mysql_connect('localhost',$usuario_mysql, $senha_mysql);
+        $con=mysql_select_db($banco_mysql ,$id);
+        $sql = "SELECT rn1, counter FROM portabilidade  WHERE numero = '$numero' LIMIT 1";
+        $result = mysql_query($sql);
+        $row = mysql_fetch_array($result);
+        if(is_array($row)){
+            if($row['counter'] < $conta){
+               $rn1 = $row['rn1'];
+               $sql = "UPDATE portabilidade SET counter = counter + 1 WHERE numero = '$numero'";
+               $result = mysql_query($sql);
+	       $cost = '(BANCO DE DADOS LOCAL)';
+        } else
+            {
+
+		$ch = curl_init();
+                curl_setopt($ch, CURLOPT_URL, "http://consulta-operadora.fluxoti.com/v1/consult/" . $numero);
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                curl_setopt($ch, CURLOPT_HTTPHEADER, array("X-Auth-Token: $token"));
+                $response = curl_exec($ch);
+                $response = json_decode($response, true);
+                $rn1 = $response['data']['rn1'] ;
+                $sql = "UPDATE portabilidade SET numero = '$numero', rn1 = '$rn1', counter = 1 WHERE numero = '$numero'";
+                $result = mysql_query($sql);
+                $cost = 'SERVIDOR - ATUALIZADO O BANCO LOCAL';
+		curl_close($ch);
+
+          }
+
+}
+else
+{
+
+	        $ch = curl_init();
+                curl_setopt($ch, CURLOPT_URL, "http://consulta-operadora.fluxoti.com/v1/consult/" . $numero);
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                curl_setopt($ch, CURLOPT_HTTPHEADER, array("X-Auth-Token: $token"));
+                $response = curl_exec($ch);
+                $response = json_decode($response, true);
+                $rn1 = $response['data']['rn1'] ;
+                $sql = "INSERT INTO portabilidade (numero, rn1, counter) VALUES ('$numero', '$rn1', 1)";
+                $result = mysql_query($sql);
+                $cost = 'SERVIDOR - ATUALIZADO O BANCO LOCAL';
+                curl_close($ch);
+
+}
+
+$agi->verbose("Consulta via: " . $cost);
+$agi->set_variable("RN1", $rn1);
+
+
+mysql_close();
+exit();
+?>
+
